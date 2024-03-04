59f635810222b6d142088d9c0cd813214c8f333c
James Almada <jamesalmada4@gmail.com>
2024-03-04 14:11:44 -0300

    atualizando o arquivo index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
-const seta = document.getElementById('seta');
+// const seta = document.getElementById('seta');
 
-seta.addEventListener('click', function(event){
-    event.preventDefault();
-})
+// seta.addEventListener('click', function(event){
+//   event.preventDefault();
+// })
